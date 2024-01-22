bf77c89d8eec75f9e7abf733bc0dcbf5f761298d
shahzaman900 <shahzamanlive2@gmail.com>
2024-01-22 15:52:07 +0500

    working on header

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css'
 import Header from './components/header'
-import { ModeToggle } from './components/mode-toggle'
 import { ThemeProvider } from './components/theme-provider'
 import { Button } from './components/ui/button'
 
@@ -8,9 +7,7 @@ function App() {
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Button>click me</Button>
-      <ModeToggle></ModeToggle>
-      <div className='bg-primary'>
+      <div>
         <Header></Header>
       </div>
     </ThemeProvider>
--- portfolio/src/components/header.tsx
+++ portfolio/src/components/header.tsx
@@ -1,8 +1,16 @@
+import profile from '../assets/profile.svg'
+import { ModeToggle } from './mode-toggle';
+import { Button } from './ui/button';
+
 function Header() {
   return (
-    <>
-      <h1 className='text-primary-foreground'>header</h1>
-    </>
+    <div className='flex justify-between pl-16 pr-16 pt-3 pb-3 '>
+      <div>
+        <img src={profile} alt="profile" />
+      </div>
+      <div></div>
+      <ModeToggle></ModeToggle>
+    </div>
   )
 }
 
