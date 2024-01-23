61b98da4bdc0547391a417e0a922bd831cb4deb3
shahzaman900 <shahzamanlive2@gmail.com>
2024-01-23 16:30:09 +0500

    make profile icon in center

--- portfolio/src/components/header.tsx
+++ portfolio/src/components/header.tsx
@@ -5,8 +5,8 @@ import { Button } from './ui/button';
 function Header() {
   return (
     <div className='flex items-center justify-between px-24 pt-3 pb-3 border-b-2 border-dotted '>
-      <div>
-        <img src={profile} alt="profile" />
+      <div className='px-5'>
+        <img src={profile} alt="profile"  />
       </div>
       <nav className='flex items-center gap-6 text-sm'>
         <a href="#" className='transition-colors hover:text-foreground/80 text-foreground/60'>
