a443eed76d5033c2fcf3793ca6dc67ce3ccfd4d4
shahzaman900 <shahzamanlive2@gmail.com>
2024-01-23 16:24:14 +0500

    add social media component

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './components/header'
+import SocialMedia from './components/socialMedia'
 import { ThemeProvider } from './components/theme-provider'
 
 function App() {
@@ -8,6 +9,12 @@ function App() {
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div>
         <Header></Header>
+        <div className='flex flex-row px-24'>
+          <SocialMedia></SocialMedia>
+          <div className='flex flex-1'>
+            <h1>body</h1>
+          </div>
+        </div>
       </div>
     </ThemeProvider>
   )
