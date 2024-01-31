004a4a794d21e983654427c002c637e2635a0c41
shahzaman900 <shahzamanlive2@gmail.com>
2024-01-31 18:31:38 +0500

    complete experience section

--- portfolio/src/components/projectCard.tsx
+++ portfolio/src/components/projectCard.tsx
@@ -6,9 +6,9 @@ function ProjectCard() {
         <h5>@nextjs</h5>
         <p className='text-foreground/60 text-sm'>The React Framework - bordermaintained by @vercel and maintained by @vercel and maintained by @vercel</p>
         <div className='flex gap-4'>
-          <p className='px-2 py-1 hover:bg-primary hover:text-primary-foreground rounded-md text-sm border'>React js</p>
-          <p className='px-2 py-1 hover:bg-primary hover:text-primary-foreground rounded-md text-sm border'>Ruby & rails</p>
-          <p className='px-2 py-1 hover:bg-primary hover:text-primary-foreground rounded-md text-sm border'>Postgres</p>
+          <p className='px-2 py-1 hover:bg-primary hover:text-white rounded-md text-sm border'>React js</p>
+          <p className='px-2 py-1 hover:bg-primary hover:text-white rounded-md text-sm border'>Ruby & rails</p>
+          <p className='px-2 py-1 hover:bg-primary hover:text-white rounded-md text-sm border'>Postgres</p>
         </div>
       </div>
     </>
--- portfolio/src/pages/experience.tsx
+++ portfolio/src/pages/experience.tsx
@@ -1,12 +1,23 @@
+import ExperienceComp from '@/components/experienceComp';
 import SocialMedia from '@/components/socialMedia';
+
+// interface Experience {
+//   date: string,
+//   title: string,
+//   company: string,
+//   worklist: string[],
+//   tags: string[]
+// }
+
+
 
 function Experience() {
   return (
     <>
-      <div className='flex flex-row px-24'>
+      <div className='flex flex-row pl-24 '>
         <SocialMedia></SocialMedia>
-        <div className='flex flex-1 items-center justify-center'>
-          <h1>Experience</h1>
+          <div className='flex flex-1 p-4 pt-8 flex-col items-center justify-start gap-8 pb-20 overflow-auto h-dvh'>
+            <ExperienceComp />
         </div>
       </div>
     </>
