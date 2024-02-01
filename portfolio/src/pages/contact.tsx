0d5c94a70a46fe35da20ae59dcaa3df45841d8d5
shahzaman900 <shahzamanlive2@gmail.com>
2024-02-01 18:06:04 +0500

    all responsive

--- portfolio/src/pages/contact.tsx
+++ portfolio/src/pages/contact.tsx
@@ -6,7 +6,7 @@ function Contact() {
     <>
       <div className='flex flex-row md:pl-24 '>
         <SocialMedia></SocialMedia>
-        <div className='flex flex-col flex-1 items-center justify-center p-4 pb-24 overflow-auto h-dvh'>
+        <div className='flex flex-col flex-1 items-start md:items-center sm:items-center justify-center p-4 md:py-0 py-24 overflow-auto h-dvh'>
           <div className='flex flex-col justify-center gap-4'>
             <h1>Contact</h1>
             <p className='text-foreground/60'>I'm always interested in hearing about new projects, so if you'd like to chat please get in touch.</p>
